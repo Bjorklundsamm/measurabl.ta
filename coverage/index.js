6dfc6ef2320fda1234a58166edc3321c3fdcde0a
Samuel Bjorklund <68446801+Bjorklundsamm@users.noreply.github.com>
2021-08-01 14:23:02 -0700

    Delete coverage directory

--- coverage/index.js
+++
@@ -1,6 +0,0 @@
-describe('Bare Minimum Requirements', function() {
-    require('./Pen.test.js');
-    require('./Recieve.test.js');
-    require('./Render.test.js');
-    require('./bare_minimum/basicChaining.test.js');
-  });
